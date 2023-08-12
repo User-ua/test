24c933764ba8fc5902a2a5c0f4b95a5e117c52d9
Ivan <ivanruzhytskyy@gmail.com>
2023-08-12 23:50:17 +0300

    Update 2023-08-12T20:50:10.586Z

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-
-const openButtonElement = document.querySelector(".button--menu");
-const openNavElement = document.querySelector(".nav");
-const buttonClickHandler = function() {
-    openNavElement.classList.toggle("nav--open"); 
-    openButtonElement.classList.toggle("button--open");     
-}
-
-openButtonElement.addEventListener("click", buttonClickHandler);
